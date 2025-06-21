befd7777a815356c1277678a444a4f48fca33d2b
Fazlu601 <fazrlu9575@gmail.com>
2025-06-21 17:52:14 +0700

    [FIX] modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from mlflow.models.signature import infer_signature
 
-mlflow.set_tracking_uri("file:///mlruns")  # Paksa gunakan path lokal di Linux
+mlflow.set_tracking_uri("mlruns") 
 
 # Argument parser
 parser = argparse.ArgumentParser()
